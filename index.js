2bad46229c4cd29561817a3be710ceb621939410
Connor Bode <connor.bode@gmail.com>
2015-07-16 10:31:33 -0400

    set up the router

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var Strongo = {};
+var StrongoRouter = require('./lib/router.js');
+
+var Strongo = function (db) {
+  var db = db;
+  this.__name__ = 'Strongo';
+
+  this.collection = function (name) {
+    var collection = db.collection(name);
+    return new StrongoRouter(collection);
+  };
+};
 
 module.exports = Strongo;
---
+++ test/test.js
@@ -0,0 +1,6 @@
+var assert = require('assert');
+describe('test', function () {
+  it('should test', function () {
+    assert.equal(1, 1);
+  });
+});
