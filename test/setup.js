7bb114ecc69435706cd186428b5dc691df365795
Connor Bode <connor.bode@gmail.com>
2015-07-18 14:17:23 -0400

    fix test setup

--- test/setup.js
+++ test/setup.js
@@ -1,2 +1,49 @@
+var colors = require('colors');
+var mongodb = require('mongodb');
+var Promise = require('es6-promise').Promise;
+var MongoClient = mongodb.MongoClient;
 global.chai = require('chai');
 global.should = chai.should();
+
+if (!process.env.MONGO_URL) {
+  var error = 'MONGO_URL environment variable must be defined to run tests';
+  console.log(error.red);
+  console.log('bye.');
+  console.log();
+  process.exit(0);
+}
+
+var handleDbConnected = function (db) {
+  global.db = db;
+  return db.createCollection('test');
+};
+
+var handleCollectionCreated = function (collection) {
+  global.collection = collection;
+  return Promise.resolve();
+};
+
+beforeEach(function (done) {
+  MongoClient
+    .connect(process.env.MONGO_URL)
+    .then(handleDbConnected)
+    .then(handleCollectionCreated)
+    .then(done)
+    .catch(function (err) {
+      throw err;
+    });
+});
+
+var closeDb = function () {
+  db.close();
+  return Promise.resolve();
+};
+
+afterEach(function (done) {
+  db.dropDatabase()
+    .then(closeDb)
+    .then(done)
+    .catch(function (err) {
+      throw err;
+    });
+});
