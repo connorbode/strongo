e84808f1e360673bfb0e2e445108fc6672aa2477
Connor Bode <connor.bode@gmail.com>
2015-07-19 16:18:50 -0400

    limit number of docs returned

--- test/lib/query.js
+++ test/lib/query.js
@@ -24,4 +24,58 @@ describe('query', function () {
       .then(queryDocs)
       .catch(helpers.catch);
   });
+
+  it('retrieves a limited number of documents', function (done) {
+    var docs = [
+      { num: 1 },
+      { num: 2 },
+      { num: 3 }
+    ];
+
+    var queryDocs = function (results) {
+      var results = results.ops.map(function (op) {
+        op._id = op._id.toString();
+        return op;
+      });
+
+      results.pop();
+
+      request
+        .get('/test?limit=2')
+        .expect(results)
+        .expect(200, done);
+    };
+
+    collection 
+      .insertMany(docs)
+      .then(queryDocs)
+      .catch(helpers.catch);
+  });
+
+  it('retrieves a documents by an offset', function (done) {
+    var docs = [
+      { num: 1 },
+      { num: 2 },
+      { num: 3 }
+    ];
+
+    var queryDocs = function (results) {
+      var results = results.ops.map(function (op) {
+        op._id = op._id.toString();
+        return op;
+      });
+
+      results.splice(0, 1);
+
+      request
+        .get('/test?offset=1')
+        .expect(results)
+        .expect(200, done);
+    };
+
+    collection
+      .insertMany(docs)
+      .then(queryDocs)
+      .catch(helpers.catch);
+  });
 });
